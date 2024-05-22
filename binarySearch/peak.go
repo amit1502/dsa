7bddf52dc816a582434d373f71bc2025820c53ef
amit1502 <amitpandey18051996@gmail.com>
2024-05-22 12:11:20 +0530

    Binary Search - Update pek

--- binarySearch/peak.go
+++ binarySearch/peak.go
@@ -4,8 +4,7 @@ func Peak(arr []int) int {
 	start, end := 0, len(arr)-1
 	for start <= end {
 		mid := (start + end) / 2
-		if (mid == 0 || arr[mid] >= arr[mid-1]) && (mid < len(arr)-1 ||
-			arr[mid] >= arr[mid+1]) {
+		if (mid == 0 || arr[mid] >= arr[mid-1]) && (mid == len(arr)-1 || arr[mid] >= arr[mid+1]) {
 			return mid
 		}
 		if mid > 0 && arr[mid-1] >= arr[mid] {
